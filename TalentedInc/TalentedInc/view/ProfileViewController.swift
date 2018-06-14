24b91a71862a7b0a9d010470bc9d9cd3889ec454
Mina Yousry <mina.yousry.samir@gmail.com>
2018-06-14 14:48:54 +0300

    starting profile design

---
+++ TalentedInc/TalentedInc/view/ProfileViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ProfileViewController.swift
+//  TalentedInc
+//
+//  Created by Admin on 6/13/18.
+//  Copyright © 2018 Admin. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UITableViewController, UICollectionViewDelegate,UICollectionViewDataSource {
+    
+    @IBOutlet var interestsCollectionView: UICollectionView!
+    
+     @IBOutlet var profileTableView: UITableView!
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "interestCell", for: indexPath)
+        
+        return cell
+    }
+    
+    
+   
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        profileTableView.separatorStyle = UITableViewCellSeparatorStyle.none
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
